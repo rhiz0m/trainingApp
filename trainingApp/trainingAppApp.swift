6bb901bfd28d685d3ec91e8280998a04e3eb39c8
taokyone <antonsson.andreas@hotmail.com>
2023-10-06 19:08:50 +0200

    Initial Commit

---
+++ trainingApp/trainingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  trainingAppApp.swift
+//  trainingApp
+//
+//  Created by Andreas Antonsson on 2023-10-06.
+//
+
+import SwiftUI
+
+@main
+struct trainingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

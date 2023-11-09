cfe4bbc56a7d19989394e7fc67d910f1fb6457c3
taokyone <antonsson.andreas@hotmail.com>
2023-11-09 17:28:05 +0100

    working on API

--- trainingApp/Model/ExerciceAPI.swift
+++ trainingApp/Model/ExerciceAPI.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+struct ExerciseAPI: Codable, Identifiable {
+    var id: UUID?
+    let name: String
+    let type: String
+    let muscle: String
+    let equipment: String
+    let difficulty: String
+    let instructions: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, type, muscle, equipment, difficulty, instructions
+    }
+    
+   /* init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        // If "id" is present in the JSON, decode it; otherwise, set it to a default UUID
+        id = try? container.decode(UUID.self, forKey: .id) ?? UUID()
+        name = try container.decode(String.self, forKey: .name)
+        type = try container.decode(String.self, forKey: .type)
+        muscle = try container.decode(String.self, forKey: .muscle)
+        equipment = try container.decode(String.self, forKey: .equipment)
+        difficulty = try container.decode(String.self, forKey: .difficulty)
+        instructions = try container.decode(String.self, forKey: .instructions)
+    } */
+}
+
--- trainingApp/View/Screens/SearchView.swift
+++ trainingApp/View/Screens/SearchView.swift
@@ -1,18 +1,94 @@
-//
-//  SearchView.swift
-//  trainingApp
-//
-//  Created by Andreas Antonsson on 2023-11-08.
-//
-
 import SwiftUI
 
+
 struct SearchView: View {
+    @StateObject private var viewModel = SearchViewModel()
+    @State private var title = ""
+    //@State private var apiResponse: String = ""
+    
     var body: some View {
-        Text("Search View!")
+        content
+    }
+    
+    @ViewBuilder private var content: some View {
+        ZStack {
+            backgroundImageView(imageName: "gym_loginBg")
+            VStack(spacing: CGFloat(GridPoints.x3)) {
+                searchFeildView
+                resultListView()
+            }
+        }
+    }
+    
+    
+    @ViewBuilder private func backgroundImageView(imageName: String) -> some View {
+        Image(imageName)
+            .resizable()
+            .scaledToFill()
+            .edgesIgnoringSafeArea(.bottom)
+            .overlay(
+                LinearGradient(
+                    gradient: Gradient(
+                        colors: [
+                            Color.indigo.opacity(0.5),
+                            Color.black.opacity(0.9)]
+                    ),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                .edgesIgnoringSafeArea(.bottom)
+            )
+    }
+    
+    @ViewBuilder private func resultListView() -> some View {
+        VStack {
+            List(viewModel.apiResponse) { exerciseInfo in
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Name: \(exerciseInfo.name)")
+                        .foregroundColor(.black)
+                    Text("Type: \(exerciseInfo.type)")
+                        .foregroundColor(.black)
+                    
+                    Text("Muscle: \(exerciseInfo.muscle)")
+                        .foregroundColor(.black)
+
+                    Text("Equipment: \(exerciseInfo.equipment)")
+                        .foregroundColor(.black)
+                    
+                    Text("Muscle: \(exerciseInfo.difficulty)")
+                        .foregroundColor(.black)
+                    
+                    Text("Muscle: \(exerciseInfo.instructions)")
+                        .foregroundColor(.black)
+                }
+            }.padding()
+            .listStyle(PlainListStyle())
+        }
+        .padding()
+    }
+
+
+
+
+    
+    @ViewBuilder private var searchFeildView: some View {
+        VStack {
+            CustomTextField(textInput: $title, title: "musclegroup", onPress: {})
+            
+            PrimaryBtn(title: "Search Muscles", onPress: {
+                viewModel.API(muscle: title)
+            })
+            
+
+        
+            }
+        }
+    }
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
     }
 }
 
-#Preview {
-    SearchView()
-}
+

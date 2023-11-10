242d5e4515092bc5cef32e9ca1a52dec3a924e44
taokyone <antonsson.andreas@hotmail.com>
2023-11-10 12:14:22 +0100

    network call test

--- trainingApp/ViewModel/SearchViewModel.swift
+++ trainingApp/ViewModel/SearchViewModel.swift
@@ -1,8 +1,45 @@
-//
-//  SearchViewModel.swift
-//  trainingApp
-//
-//  Created by Andreas Antonsson on 2023-11-09.
-//
+
 
 import Foundation
+
+class SearchViewModel: ObservableObject {
+    @Published var apiResponse: [ExerciseAPI] = []
+    
+    func API(muscle: String) {
+        guard let muscleEncoded = muscle.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+              let url = URL(string: "https://api.api-ninjas.com/v1/exercises?muscle=\(muscleEncoded)") else {
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.setValue(auth().API_KEY, forHTTPHeaderField: "X-Api-Key")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else { return }
+            
+            do {
+                let decoder = JSONDecoder()
+                let result = try decoder.decode([ExerciseAPI].self, from: data)
+                
+                let exerciseDetails = result.map { exercise in
+                    return ExerciseAPI(
+                        id: UUID(),
+                        name: exercise.name,
+                        type: exercise.type,
+                        muscle: exercise.muscle,
+                        equipment: exercise.equipment,
+                        difficulty: exercise.difficulty,
+                        instructions: exercise.instructions
+                    )
+                }
+                
+                DispatchQueue.main.async {
+                    self.apiResponse = exerciseDetails
+                }
+            } catch {
+                print("Error decoding JSON: \(error)")
+            }
+        }
+        task.resume()
+    }
+}
--- trainingAppTests/networkCallTest.swift
+++ trainingAppTests/networkCallTest.swift
@@ -8,29 +8,60 @@
 import XCTest
 @testable import trainingApp
 
-final class trainingAppTests: XCTestCase {
-
+final class networkCallTest: XCTestCase {
+    
+    private var sut: SearchViewModel!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        sut = SearchViewModel()
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        sut = nil
     }
-
+    
+    func testNetworkCall() throws {
+        
+        let expectation = XCTestExpectation(description: "API request")
+        
+        let expectedMuscle = "biceps" // testing the network call
+        
+        sut.API(muscle: expectedMuscle)
+        
+        // Wait for the asynchronous task to complete
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            
+            // Check if the apiResponse has been updated
+            XCTAssertFalse(self.sut.apiResponse.isEmpty)
+            
+            XCTAssertTrue(self.sut.apiResponse.allSatisfy { $0.muscle == expectedMuscle })
+            
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 10)
+    }
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        
+        XCTAssertEqual(sut.apiResponse.count, 0, "Initially, expecting apiResponse to be empty")
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
+            for _ in 0..<10 {
+                sut.API(muscle: "hamstrings") // testing performance of making 10 API calls
+            }
         }
     }
-
 }
